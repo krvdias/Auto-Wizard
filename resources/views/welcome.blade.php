d303deee49a6c01d0657e1dcf79996435a6160c2
Nimsara Thamel <nimsarathamel@gmail.com>
2024-01-17 08:40:33 +0530

    frontend 2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,25 +7,94 @@
         <title>Auto Wizards - Dashboard</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="CSS/custom.css"/>
+        <link rel="stylesheet" href="{{asset('CSS/custom.css')}}"/>
 
     </head>
-    <body class="antialiased">
-        <div class="col 6 dashBoard">
-            <h1>Dashboard</h1>
-             <ul>
-                <li><a href="/homePage">Home page</a>
-                <li><a href="/servicePage">Service</a>
-                <li><a href="/contactPage">Contact Us</a>
-                <li><a href="/signInPage">Sign In/Register</a>
-             </ul>
+   
+    <body>
+        <!-- Navbar Start -->
+        <div class="container-fluid bg-transparent">
+            <div class="container">
+                <nav class="navbar navbar-dark navbar-expand-lg py-lg-0">
+                    <a href="index.html" class="navbar-brand">
+                        <h1 class="text-primary mb-0 display-5">Auto<span class="text-white">Wizard</span><i class="fa fa-spider text-primary ms-2"></i></h1>
+                    </a>
+                    <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="fa fa-bars text-dark"></span>
+                    </button>
+                    <div class="collapse navbar-collapse me-n3" id="navbarCollapse">
+                        <div class="navbar-nav ms-auto">
+                            <a href="index.html" class="nav-item nav-link active">Home</a>
+                            <a href="about.html" class="nav-item nav-link">About</a>                        
+                            <a href="project.html" class="nav-item nav-link">Dashboard</a>
+                            <div class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Services</a>
+                                <div class="dropdown-menu m-0 bg-primary">
+                                    <a href="price.html" class="dropdown-item">Service 01</a>
+                                    <a href="blog.html" class="dropdown-item">Service 02</a>
+                                    <a href="team.html" class="dropdown-item">Service 03</a>
+                                    <a href="testimonial.html" class="dropdown-item">Service 04</a>
+                                </div>
+                            </div>
+                            <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
-        <div class="col 6">
+        <!-- Navbar End -->
+
+        <!-- Carousel Start -->
+        <div class="container-fluid carousel px-0 mb-5 pb-5">
+            <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+                    <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active" style="background-image: url(storage/automobile1.png);">
+                        <div class="carousel-caption">
+                            <div class="container carousel-content">
+                                <h4 class="text-white mb-4 animated slideInDown">Expert Service, Guaranteed Satisfaction</h4>
+                                <h1 class="text-white display-1 mb-4 animated slideInDown">YOUR CAR'S TRUSTED COMPANION</h1>
+                                <a href="" class="me-2"><button type="button" class="px-5 py-3 btn btn-primary border-2 rounded-pill animated slideInDown">Sign In</button></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item" style="background-image: url(storage/automobile2.png);">                       
+                        <div class="carousel-caption">
+                            <div class="container carousel-content">
+                                <h4 class="text-white mb-4 ">Expert Service, Guaranteed Satisfaction</h4>
+                                <h1 class="text-white display-1 mb-4 ">YOUR CAR DESERVES THE BEST. GIVE IT TO US.</h1>
+                                <a href="" class="me-2"><button type="button" class="px-5 py-3 btn btn-primary border-2 rounded-pill animated slideInDown">Sign In</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev btn btn-primary border border-2 border-start-0 border-primary" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next btn btn-primary border border-2 border-end-0 border-primary" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
+        <!-- Carousel End -->
 
 
+        <!-- JavaScript Libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
-
+        <!-- Template Javascript -->
+    <script src="{{asset ('JS/jquery.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     </body>
